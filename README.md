a4d6eec7e8096576a9ee62c350e74a23187cf61b
Arthur Ryabtsev <ryabtsev.art@gmail.com>
2018-09-24 16:57:00 -0700

    force push

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
 # Poker_Face
 A simplified browser poker game.
+
+### Getting Started
+
+Follow these steps in order to get a copy of a game to run on your local machine.
+
+### Prerequisites
+
+This application requires [Node] to run the server. I recommend installing it with [Homebrew].
+
+### Installation
+
+Clone or download the project onto your local machine.
+
+Install dependencies.
+```
+npm install
+```
+Start the server
+```
+npm start
+```
+Now you are ready to play a simplified poker game! 
+
+Open your browser and go to `localhost:4444` and the app will launch. 
+
+## Testing
+
+In order to test the application, you will need to manually overwrite 'babel-core' to '@Babel/core' in the node_modules directory, mentioned in an error. 
+
+I am currently working to solve this issue. 
+
+Run the tests with
+```
+npm run test
+```
+
+## Author
+Arthur Ryabtsev.
--- client/components/App/App.jsx
+++ client/components/App/App.jsx
@@ -1,18 +1,42 @@
 import React, { Component } from 'react';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import * as actions from '../../actions/index.js';
 
-class App extends Component {
+import Hand from '../Hand/Hand.jsx';
+import Button from '../Button/Button.jsx';
 
-  constructor(props) {
-    super(props);
-  };
+// Assigns state values to a component
+
+const mapStateToProps = (state) => {
+  return {
+    deck: state.deck,
+    hand: state.hand,
+    game: state.game,
+    score: state.score
+  }
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return bindActionCreators(actions, dispatch);
+};
+
+let gameModes = ['straight', 'pair', 'not a winner', 'ready'];
+
+class Main extends Component {
 
   render() {
+    let props = this.props.game
     return (
-      <div>
-        This is the main App component.
+      <div className="App">
+        <Hand {...this.props} />
+        <Button />
+        <div>
+        {gameModes.includes(this.props.game) && this.props.game}!
+        </div>
       </div>
     )
   }
 }
 
-export default App;
+export default connect(mapStateToProps, mapDispatchToProps)(Main);
